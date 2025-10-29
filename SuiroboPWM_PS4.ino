c5f5ad16ea8b7eeffe7bf82a568095a84df38c3c
eyunya11 <eyunya1126@icloud.com>
2025-10-29 15:58:56 +0900

    見た目きれいにした

--- SuiroboPWM_PS4.ino
+++ SuiroboPWM_PS4.ino
@@ -9,18 +9,21 @@ int speedM = 160;
 int speedS = 0;
 int speedL = 100;
 //初期設定
-void setup() {
+void setup()
+{
   Serial.begin(115200);//シリアル通信
   PS4.begin("80:f3:da:41:53:de");//コントローラーのMACアドレスを指定
   Serial.println("Ready.");
   //PWMの設定
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++)
+  {
     ledcSetup(i, 12800, 8);
     ledcAttachPin(In[i], i);
   }
 }
 
-void loop() {
+void loop()
+{
   Serial.println("MD1"); 
   MD1(PS4.LStickX(),PS4.RStickX());//MD1の制御
   MD2(PS4.L2(),PS4.R2()); //MD2の制御
@@ -30,7 +33,8 @@ void loop() {
   delay(50);
 }
 //1モーター制御関数
-void MD1(int left,int right){
+void MD1(int left,int right)
+{
     if(left > 20)
     {
       ledcWrite(0, left);
@@ -41,7 +45,8 @@ void MD1(int left,int right){
       ledcWrite(0, 0);
       ledcWrite(1, abs(left));
     }
-    else{
+    else
+    {
       ledcWrite(0, 0);
       ledcWrite(1, 0);
     }
@@ -55,13 +60,15 @@ void MD1(int left,int right){
       ledcWrite(2, 0);
       ledcWrite(3, abs(right));
     }
-    else{
+    else
+    {
       ledcWrite(2, 0);
       ledcWrite(3, 0);
     }
 }
 //2モーター制御関数
-void MD2(int left,int right){
+void MD2(int left,int right)
+{
     if(left > 20)
     {
       ledcWrite(4, left + 127);
@@ -71,7 +78,9 @@ void MD2(int left,int right){
     {
       ledcWrite(4, 0);
       ledcWrite(5, abs(left) + 127);
-    }else{ 
+    }
+    else
+    { 
       ledcWrite(4, 0);
       ledcWrite(5, 0);
     }
@@ -84,9 +93,10 @@ void MD2(int left,int right){
     {
       ledcWrite(6, 0);
       ledcWrite(7, abs(right) + 127);
-    }else{
+    }
+    else
+    {
       ledcWrite(6, 0);
       ledcWrite(7, 0);
     }
-    
 }
